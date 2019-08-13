efff01e96fabd2de13cd109e658126d7d48df623
pierrereffay <pierre.reffay@gmail.com>
2019-08-13 15:29:30 +0200

    exo 2 sur le MVC sans ressources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # # Read all
+  get '/tasks', to: 'tasks#index'
+
+# # Create
+  get '/tasks/new', to: 'tasks#new', as: :new
+  post '/tasks', to: 'tasks#create'
+
+  # # Read one
+  get '/tasks/:id', to: 'tasks#show', as: :show_task
+
+  # Update
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch '/tasks/:id', to: 'tasks#update', as: :task
+
+  # Delete
+  delete '/tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
